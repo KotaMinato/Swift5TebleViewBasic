e5e9394e2a8e973a048117cfdb0b6043d8159c28
kota0207 <minato.innetwork@gmail.com>
2021-03-06 12:58:48 +0900

    Initial Commit

---
+++ Swift5TebleViewBesic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift5TebleViewBesic
+//
+//  Created by minato on 2021/03/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
